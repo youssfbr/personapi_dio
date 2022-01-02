f2831e0a9260e8402ee4e7532e50aa3920bba20b
Alisson Youssf <youssfbr@gmail.com>
2022-01-02 11:44:47 -0300

    Object-Relational Mapping (ORM)

--- backend/src/main/java/com/github/youssfbr/personapi/model/entities/Person.java
+++ backend/src/main/java/com/github/youssfbr/personapi/model/entities/Person.java
@@ -5,24 +5,32 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false, unique = true)
     private String cpf;
 
     private LocalDate birthDate;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
 }
--- backend/src/main/java/com/github/youssfbr/personapi/model/entities/Phone.java
+++ backend/src/main/java/com/github/youssfbr/personapi/model/entities/Phone.java
@@ -2,17 +2,27 @@ package com.github.youssfbr.personapi.model.entities;
 
 import com.github.youssfbr.personapi.model.entities.enums.PhoneType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
 
+    @Column(nullable = false)
     private String number;
 }
