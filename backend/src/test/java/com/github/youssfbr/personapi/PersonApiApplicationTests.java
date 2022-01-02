61fad1c7950e6c254ab9dc8ec055427586c2f5a2
Alisson Youssf <youssfbr@gmail.com>
2022-01-02 00:37:38 -0300

    Project created

---
+++ backend/src/test/java/com/github/youssfbr/personapi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
