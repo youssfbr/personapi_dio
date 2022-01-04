de61a84b21f46687a06c0b4ebd171a2faf60cf8e
Alisson Youssf <youssfbr@gmail.com>
2022-01-04 09:49:23 -0300

    Updated README.md file

--- backend/src/main/java/com/github/youssfbr/personapi/services/PersonService.java
+++ backend/src/main/java/com/github/youssfbr/personapi/services/PersonService.java
@@ -10,8 +10,9 @@ import com.github.youssfbr.personapi.services.exceptions.DatabaseException;
 import com.github.youssfbr.personapi.services.exceptions.InternalServerError;
 import com.github.youssfbr.personapi.services.exceptions.PersonNotFoundException;
 import com.github.youssfbr.personapi.services.interfaces.IPersonService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ import java.util.stream.Collectors;
 
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService implements IPersonService {
 
     private final IPersonRepository personRepository;
